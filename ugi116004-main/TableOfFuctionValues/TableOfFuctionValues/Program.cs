6764c4e1ce1239a1137d742c529349e391d62ad7
Будаев Иван <budaevivan2003@gmail.com>
2022-06-22 20:12:06 +0500

    закончил задание 13

--- ugi116004-main/TableOfFuctionValues/TableOfFuctionValues/Program.cs
+++ ugi116004-main/TableOfFuctionValues/TableOfFuctionValues/Program.cs
@@ -8,32 +8,46 @@ namespace TableOfFuctionValues
         {
             Console.WriteLine("Введите начальное значение аргумента");
             var x0 = double.Parse(Console.ReadLine());
-            
+
             Console.WriteLine("Введите значение шага");
             var step = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите число строк таблицы");
             var n = int.Parse(Console.ReadLine());
 
-            PrintHeader("x^2");
+            //PrintHeader("x^2");
 
-            for(var i = 0; i < n; i++)
-            {
-                var x = x0 + step * i;
-                Console.WriteLine($"{x,5:F2} | {x * x:F3}");
-            }
+            //for(var i = 0; i < n; i++)
+            //{
+            //    var x = x0 + step * i;
+            //    Console.WriteLine($"{x,5:F2} | {x * x:F3}");
+            //}
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            PrintHeader("sin x");
+            //PrintHeader("sin x");
 
-            for (var i = 0; i < n; i++)
-            {
-                var arg = x0 + step * i;
-                Console.WriteLine($"{arg,5:F2} | {Math.Sin(arg):F5}");
-            }
+            //for (var i = 0; i < n; i++)
+            //{
+            //    var arg = x0 + step * i;
+            //    Console.WriteLine($"{arg,5:F2} | {Math.Sin(arg):F5}");
+            //}
+
+            PrintTable("x^2", x0, step, n, x => x * x);
 
             Console.ReadKey();
+        }
+
+        static void PrintTable(string name, double start, double step, int rowsNumber, Func<double, double> f)
+        {
+            PrintHeader(name);
+
+            for(var i = 0; i < rowsNumber; i++)
+            {
+                var arg = start + i * step;
+                Console.WriteLine($"{arg,5:F2} | f{arg}:F4");                
+            }
+            Console.WriteLine();
         }
 
         static void PrintHeader(string functionName)
